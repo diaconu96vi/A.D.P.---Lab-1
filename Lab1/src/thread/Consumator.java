493fda706cdee464a4231b327aca12fa5a9f9c9f
Diaconu Ionut-Victor <36921680+ryousenka@users.noreply.github.com>
2018-03-06 17:15:01 +0200

    Update Consumator.java

--- Lab1/src/thread/Consumator.java
+++ Lab1/src/thread/Consumator.java
@@ -3,23 +3,22 @@ package thread;
 public class Consumator extends Thread {
 	
 	private Producator producator;
-	int elementToConsume;
 	@Override
 	public void run() {
 		try {
 			while(true) {
-				elementToConsume = producator.randomElement;
+				Main.s.acquire();
 				if(Main.list.isEmpty() == true) {
 					System.out.println("Lista este goala");
 				}
 				else {
 					System.out.println("Se consuma elementul...");
-					Main.s.acquire();
+					
 					Main.list.remove(elementToConsume);
 					System.out.println("A fost consumat elementul" + elementToConsume);
-					Main.s.release();
+					
 				}
-				
+				Main.s.release();
 				Thread.sleep(100);
 			}
 		}
